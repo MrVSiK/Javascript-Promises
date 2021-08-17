3c292bdbf7fcd14537d42e1dd88c62babbe0f96e
Srinivas Varanasi <79159341+MrVSiK@users.noreply.github.com>
2021-08-17 12:17:57 +0530

    Create Promise-Chaining.js

---
+++ Promise-Chaining.js
@@ -0,0 +1,21 @@
+let p = new Promise((resolve,reject) => {
+    if(Math.random() > 0.5){
+        resolve("I am greater than half");
+    } else {
+        reject("I am less than half")
+    }
+})
+
+p.then((resolved) => {
+    console.log("inside first .then() method --- " + resolved);
+    return "This has been resolved"
+}, (rejected) => {
+    console.log("inside first .then() method and rejectd ---" + rejected);
+    return "This has been rejected"
+}).then((resolved) => {
+    console.log("inside second .then() method --- " + resolved);
+}).catch((err) => {
+    console.log("Inside .catch() method --- " + err);
+}).finally(() => {
+    console.log("Execution has been completed");
+})
